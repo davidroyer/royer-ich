06e8c72fada4272ee58cd54164e3cee3994b81b5
David Royer <droyer01@gmail.com>
2019-11-06 23:55:52 -0500

    chore: update

---
+++ components/MarketingBlock.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row align="center" class="my-12 py-12">
+    <v-col cols="12" md="6">
+      <div class="mblock-content">
+        <h2>The secret lies within the bean</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          laboriosam rem hic nam esse dolorem, nemo vero commodi maiores facere.
+        </p>
+      </div>
+    </v-col>
+
+    <v-col>
+      <img
+        class="mx-auto d-block"
+        src="@/assets/images/coffee-black.png"
+        alt=""
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass">
+.mblock-content
+  margin: 0 auto
+  max-width: 500px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <v-row class="top-banner">
+    <v-row align="center" class="top-banner">
       <v-col cols="12" md="4">
         <span>PREMIUM COFFEE</span>
         <h2>
@@ -12,10 +12,12 @@
       </v-col>
     </v-row>
 
+    <MarketingBlock class="flex-row-reverse" />
+
     <v-container>
       <v-row>
         <v-col>
-          <h1>White Tabs Section</h1>
+          <h1>Our Best Deal Section</h1>
           <v-row align="center">
             <v-col
               v-for="(coffee, index) in coffeeProducts"
@@ -29,14 +31,18 @@
         </v-col>
       </v-row>
     </v-container>
+
+    <MarketingBlock />
   </div>
 </template>
 
 <script>
 import CoffeeProduct from '@/components/CoffeeProduct'
+import MarketingBlock from '@/components/MarketingBlock'
 
 export default {
   components: {
+    MarketingBlock,
     CoffeeProduct
   },
 
